345e5e9636a1c8d6604e327dc4f6ef77d4f93b70
Erfin Feluzy <erfin.feluzy@gmail.com>
2019-05-29 13:24:59 +0700

    queue name

--- src/main/java/com/redhat/app/xlate/route/CamelRoute.java
+++ src/main/java/com/redhat/app/xlate/route/CamelRoute.java
@@ -20,7 +20,9 @@ public class CamelRoute extends RouteBuilder {
 			.to("jms:queue:q.empi.transform.dlq");
 		
 		from("jms:queue:q.input")
+			.log("received message on: jms:queue:q.email")
 			.convertBodyTo(String.class)
+			.log("received message on: jms:queue:q.output")
 			.to("jms:queue:q.output");
 		
 		
